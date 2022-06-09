40e245844405e66c757160f77a2b63885c0ec017
nitryuu <kscdh6@gmail.com>
2022-06-09 11:02:19 +0700

    add: another theme

--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,13 @@
 # OHMYPOSH
 Import-Module oh-my-posh
 oh-my-posh init pwsh --config C:/Users/nitryuu/AppData/Local/oh-my-posh/themes/powerlevel10k_rainbow.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\spaceship.omp.json" | Invoke-Expression
 
 # PSReadLine
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Fzf
 Import-Module PSFzf
